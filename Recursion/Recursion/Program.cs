3780992319d52e8dae457e9e20779ace23d8bf5a
pashaslayer <pavel.khakhlou@gmx.at>
2022-10-22 17:35:44 +0200

    Rekursive + Iterative Programmierung; Hausübung

---
+++ Recursion/Recursion/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        // Test: 
+        Console.WriteLine("Rekursive Lösung");
+        Console.WriteLine(RecursiveFactorial(4));
+        Console.WriteLine();
+        Console.WriteLine(IterativeFactroial(4));
+
+    }
+
+
+    public static int RecursiveFactorial(int n)
+    {
+        if(n == 1 || n == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * RecursiveFactorial(n - 1);
+        }
+    }
+
+    public static String IterativeFactroial(int n)
+    {   
+        int p = 1;
+        while(n > 1)
+        {
+            p = p * n;
+            n--;
+        }
+        return "Iterative Lösung " + p;
+    }
+}
